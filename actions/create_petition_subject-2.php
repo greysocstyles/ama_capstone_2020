c66b6e69c864f4f6aa9a197154e627550875bc61
greysocstyles <socratesbinos26@gmail.com>
2020-09-07 23:49:00 +0800

    fix create petition

--- actions/create_petition_subject-2.php
+++ actions/create_petition_subject-2.php
@@ -3,30 +3,11 @@
 if (isset($_POST['create_petition']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
 	$petition_subject = $_POST['petition_subject'];
 	
-	$select_exist = query("	select subject_code 
-						 	from request_to_open_list rto 
-						 	inner join subject_list sl 
-										on rto.subject_id = sl.id 
-							where subject_id = '$petition_subject' 
-						 	and status = 'Pending' or status = 'Approved'
-						");
-
-	if ($select_exist) {
-		$row_count = mysqli_num_rows($select_exist);
-
-		if ($row_count > 0) {
-			$petition_exist = array();
-			while ($row = mysqli_fetch_assoc($select_exist)) {
-				$petition_exist[] = $row;
-			}
-
-		} else {
-			$insert_petition = query("insert into request_to_open_list (subject_id) values ('$petition_subject')");
+	$insert_petition = query("insert into request_to_open_list (subject_id) values ('$petition_subject')");
 			
-			if ($insert_petition) {
-				header('Location: index.php?m=pl');
-				exit;
-			}
-		}
+	if ($insert_petition) {
+		header('Location: index.php?m=pl');
+		exit;
 	}
+			
 }
--- forms/create_petition_subject-2.form.php
+++ forms/create_petition_subject-2.form.php
@@ -1,5 +1,20 @@
 <?php require_once 'actions/create_petition_subject-2.php'; ?>
 
+<?php 
+
+$subject_list = query("select  id
+							,  subject_code 
+					from subject_list 
+					where id 
+						not in 
+						(
+							select subject_id
+							from request_to_open_list
+							where status = 'Pending'
+						) 
+					and subject_status = 0");
+
+?>
 <form action="index.php?c=cp" method="POST">
 	<div class="row">
 		<div class="col-md-8 m-auto">
@@ -9,31 +24,20 @@
 						<h3>Create Petition</h3>
 					</div>
 					<div class="form-group">
-						<label>Select Subject</label>
+						<label>Subject</label>
 						<select class="form-control" name="petition_subject">
-							<?php 
-							$subject_list = query("select id, subject_code from subject_list where subject_status = 0"); 
+							<option value="">Select Subject</option>
+							<?php
+
 							if ($subject_list):
-								while ($row = mysqli_fetch_assoc($subject_list)):
-									  ?>
-									  <option value="<?php echo $row['id'] ?>" <?php if(isset($_POST['petition_subject']) && $_POST['petition_subject'] == $row['id']) echo 'selected' ?>><?php echo $row['subject_code'] ?></option>
-									  <?php 
+								while ($row = mysqli_fetch_assoc($subject_list)): ?>
+
+									<option value="<?php echo $row['id'] ?>" <?php if(isset($_POST['petition_subject']) && $_POST['petition_subject'] == $row['id']) echo 'selected' ?>><?php echo $row['subject_code'] ?></option>
+									<?php 
 								endwhile;
 							endif;
 							?>
 						</select>
-					</div>
-					<div class="form-group">
-						<?php 
-						if (isset($petition_exist)):
-							echo 'Petition Exist: ';
-							foreach ($petition_exist as $value):
-									?>
-									<strong class="text-danger"><?php echo implode($value) ?></strong>
-									<?php
-							endforeach;
-						endif; 
-						?>
 					</div>
 					<div class="form-group">
 						<button class="btn btn-primary" name="create_petition">Submit</button>
